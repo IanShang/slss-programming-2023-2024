548aabe4905e533b9445466096d92444d785abf0
-v <vipianshang@gmail.com>
2023-09-19 11:06:22 -0700

    did some funny things

---
+++ Notes/1 - Natural Language Processing.md
@@ -0,0 +1,16 @@
+---
+notes: 
+tags:
+---
+# Output
+We can display text and symbols using output
+
+```Java
+system.out.println("Minor amounts of trolling")
+```
+```python
+print("Minor amounts of trolling")
+```
+
+# Comments
+minor amounts of useless text
---
+++ quotegenerator.py
@@ -0,0 +1,8 @@
+# art thou making words of the wisdom??
+# sun tzu
+# 43/25/1459
+#output a quote from  une personne
+print("*rudder noises - germany")
+print("AuGhGhghghhGhhgG - ian probably")
+ian_moment = input("what does ian say when he sees olifu diafutiersi:")
+print("ian says" +  ian_moment)
