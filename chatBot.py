f5ae437d2705ac787d1404080e00a9e6048c4145
-v <vipianshang@gmail.com>
2023-09-25 12:50:21 -0700

    britain moment

--- chatBot.py
+++ chatBot.py
@@ -2,6 +2,16 @@
 # quandale dingle
 # 2000 B.C.
 
-print("hello")
+# bonjour avec bread
+# steal user name, and store it
+# idk slander the user???
 
-print("This works?")
+print("i'll tell you what you fat little- 🇬🇧🇬🇧🇬🇧, ur being questioned for suspicions of a Soviet spy 🇷🇺🇷🇺🇷🇺")
+name = input("what is ur name: ")
+food = input("ok " + name + ", what food do u eat on a daily basis")
+if food == "beans":
+    tea = input("do u have tea on a daily basis")
+    if tea == "yes":
+        print("ei mate, welcome to bri'ain 🤝🤝🤝, a beautiful day outside innit")
+    else: 
+else: 
