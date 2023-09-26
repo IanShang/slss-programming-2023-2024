1c549d43f8c5a8a40653481156df77adc52f264b
-v <vipianshang@gmail.com>
2023-09-26 11:08:57 -0700

    britain gameplay buffed 6.7%

--- chatBot.py
+++ chatBot.py
@@ -2,16 +2,33 @@
 # quandale dingle
 # 2000 B.C.
 
-# bonjour avec bread
+# bonjour avec bread (how did france turn british)?
 # steal user name, and store it
-# idk slander the user???
+# idk slander the user if not british enough
 
+# le value des objects peut etre type different
+# nous utilise [] pour une liste
+# nous separate des objects avec ,
+# francais = [francais, anglais, chinois]
+# francais[0] / francais[-3]
+# dans le brakcet, nous donnons l'index de objects nous pouvons
+
+# le module de temps
+# nous pouvons utilise les temp au arrete notre code
+# import time
+# time.sleep(1) il dormir pour une second
+
+import random
+### import time #aaa ur being interrogated ur not geting time interval pauses 
+
+sus = ["*vine boom", "🤨🤨🤨", "📸📸📸", "sussy baka", "amogus"]
+ok = ["ei mate, welcome to bri'ain 🤝🤝🤝, a beautiful day outside innit", "fabulous selection of delicacies", "*sip", "*tea noises"]
 print("i'll tell you what you fat little- 🇬🇧🇬🇧🇬🇧, ur being questioned for suspicions of a Soviet spy 🇷🇺🇷🇺🇷🇺")
 name = input("what is ur name: ")
-food = input("ok " + name + ", what food do u eat on a daily basis")
+food = input("ok " + name + ", what food do u eat on a daily basis: ")
 if food == "beans":
-    tea = input("do u have tea on a daily basis")
+    tea = input("do u have tea on a daily basis: ")
     if tea == "yes":
-        print("ei mate, welcome to bri'ain 🤝🤝🤝, a beautiful day outside innit")
-    else: print("🤨🤨🤨")
-else: print("🤨🤨🤨")
+        print(ok[random.randint(0, len(ok) - 1)])
+    else: print(sus[random.randint(0, len(sus) - 1)])
+else: print(sus[random.randint(0, len(sus) - 1)])
