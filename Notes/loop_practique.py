47870a7adc55a12ebea0d1d7df9917a7edad7a66
-v <vipianshang@gmail.com>
2023-10-10 11:20:15 -0700

    funny french notes once again

---
+++ Notes/loop_practique.py
@@ -0,0 +1,32 @@
+# practique avec les loope desune n'est ce pas
+# not a noble gas (ion) (funi)
+# 2/9/1945
+
+# faire une list de objects que je dois achete a la super
+# faire une funi pour tout les objects dans le list
+# parler tres beau rue
+groceries = ["F6F Hellcat", "TB2D Skypirate", "Curtiss XSB3C", "F7F Tigercat", "USS Essex-class carriers"]
+
+for item in groceries:
+    print("what is bro eating 🗿🗿🗿")
+
+# parles star desune
+i = 0
+stars = ["*", "**", "***", "****", "*****"]
+while i <= len(stars) - 1:
+    print(stars[i])
+    i += 1
+
+# faire une math-sous que commence a 10 seconds et faire mathematics jusque 0
+# this is a certified java coder moment
+t = 10
+while t > 0:
+    if t >= 1:
+        print(t)
+        t -= 1
+print("tres bon anniversaire")
+
+# master ooooooogway coding moment
+for i in range(10, 0, -1):
+    print(i)
+print("tres bonne anniversaire")
