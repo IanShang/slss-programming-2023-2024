c7fd6e1d0f6aba83b045e4b1358907f704fcd950
-v <vipianshang@gmail.com>
2023-10-10 11:13:09 -0700

    ADDED french food idk why im yelling

--- mange.py
+++ mange.py
@@ -1,14 +1,14 @@
 # je mange les nourriture
 # niece and nephews ian shanks (Real)
-# yesterday, dec 7 1941, a date which will live in infamy - the united states of america was suddenly and deliberately attacked by the naval and air forces of the mpire of japan - F.D.R. why do i remember this
+# dec 7 1941
 
 # c'est une robob qui demander les personnes quest'que ce votre nourriture preferre
 # cette robob donne une funny textos pour cette personne
 # le robob vais dire je m'appelle
 
-cn_food = ["noodles", "dumplings", "sticky rice"]
+cn_food = ["noodles", "dumplings", "sticky rice", "moon cake"]
 jp_food = ["sushi", "miso soup", "ramen", "takoyaki"]
-fr_food = ["baguette"]
+fr_food = ["baguette", "champagne"]
 ru_food = ["vodka"]
 gb_food = ["tea", "biscuits", "beans", "toast"]
 
@@ -18,7 +18,7 @@ fav_food = input("quest'que c'est ton nourriture preferre: ")
 cn_count = 0
 while(cn_count <= len(cn_food) -1):
     if fav_food.lower().strip(" .?!") == cn_food[cn_count]:
-        print("Ching cheng Hanji, can't touch me at all. The kitchen in the dungeon. I swear I'll beat Paul the fool. *** ******** ***. my mom doesn't love me. I swear I'll hit Paul")
+        print("Ching cheng Hanji. Can't touch me at all. The kitchen in the dungeon. I swear I'll beat Paul the fool. *** ******** ***. my mom doesn't love me. I swear I'll hit Paul")
     cn_count = cn_count +1
 
 jp_count = 0
@@ -30,7 +30,7 @@ while(jp_count <= len(jp_food) -1):
 fr_count = 0
 while(fr_count <= len(fr_food) -1):
     if fav_food.lower().strip(" .?!") == fr_food[fr_count]:
-        print("mes amis, vais tu preferre un peu de champagne")
+        print("mes amis, vais tu preferre un peu de champagne ou baguette")
     fr_count = fr_count +1
 
 ru_count = 0
