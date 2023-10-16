2952408e7b80189965269412ddbc02d160c86c50
-v <vipianshang@gmail.com>
2023-10-16 11:52:13 -0700

    made star wars french

---
+++ Notes/guerreDeStar.py
@@ -0,0 +1,12 @@
+# guerre de star bot desune 
+# "hello there"
+# 9/9/999 (AD)
+
+cape = input("aimes-tu cape: ")
+if cape.lower() == "oui":
+    print("a cote de de RGB 0,0,0")
+else:
+    color = input("aimes-tu rouge: ")
+    if color.lower() == "oui":
+        print("a cote de de RGB 0,0,0")
+    else: print("a cote de de RGB 255,255,255")
