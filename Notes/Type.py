c0fabcf5225c5525ad6106b246ffd49fd486a963
-v <vipianshang@gmail.com>
2023-10-27 12:37:48 -0700

    e

---
+++ Notes/Type.py
@@ -0,0 +1,19 @@
+# group data pour types
+# str, "bonjour tout le monde"
+# list, separate avec ,
+# boolean, vrai ou faux
+# ints, quand the n%1 est 0 🗿
+# floats, quand n%1 n'est pas 0 🗿
+nouritture = "rice"
+age = 17
+
+# operator faire travailler sur numero 🤔
+# Python utilise BEDMAS 🤔
+print(420_420_420)
+print(2 ** 10)
+
+# floor division 🤔, n'ai pas un decimal, les numero aller sous
+print(9//2)
+
+# mod, returner remainder 🤔
+print(9%2)
