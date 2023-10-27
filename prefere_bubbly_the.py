a050778bb064516fe6eb3e0c7b80506ca6af82ef
-v <vipianshang@gmail.com>
2023-10-27 12:46:54 -0700

    social credit + 1000

---
+++ prefere_bubbly_the.py
@@ -0,0 +1,17 @@
+# bbt popularity
+# ian shangsssssssss why cant i hold down s
+# demander 5 personnes pour sont bbt restaurant preferee
+
+# options: chinese tea, no other options allowed 🔫
+# print summary de results
+
+chinese_tea_count = 0
+demander = input("do you like Chinese Tea? ")
+social_credit = 1000
+if demander.strip("!.,").lower(){
+    social_credit =+ 10
+    chinese_tea_count =+ 100000 # not rigged at all
+    print("your social credit score is now " + social_credit)
+    print("Chinese tea is winnning with " + chinese_tea_count + " people liking Chinese Tea")
+}
+else: print("Execution Date: 明日黎明")
