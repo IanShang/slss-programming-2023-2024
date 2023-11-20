9e6a76c44ab2e5c1b0701fe7e15968c9859e530d
-v <vipianshang@gmail.com>
2023-11-20 10:23:45 -0800

    did funny update

---
+++ number_solutions.py
@@ -0,0 +1,7 @@
+# numero n'est pas une probleme
+# dissociated ions
+# 20/11/2024
+
+# https://github.com/IanShang/slss-programming-2023-2024/blob/main/robob_parents.py - parents robob
+# https://github.com/IanShang/slss-programming-2023-2024/blob/main/memes_deux_personnes.py - memes activite
+# https://github.com/IanShang/slss-programming-2023-2024/blob/main/data_example.csv  - data par example
