df117e2a8e1941d77a556971c8f644682e6b0cfa
-v <vipianshang@gmail.com>
2023-11-24 10:24:55 -0800

    functione

---
+++ Notes/function.py
@@ -0,0 +1,9 @@
+# quest'que ce une functione?
+# une functione est une square de programme que tu peux utilise multiple fois
+# nous pouvons referee les data a parametre
+# nous utilisons "arguments" au describe data
+
+def area_de_square(s: float):
+    print("l'area de square est " + str(s*2) + " sqaured")
+
+area_de_square(2)
