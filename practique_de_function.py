3090081c65da4e3da599b58b5148f6a6afb8d1d8
-v <vipianshang@gmail.com>
2023-11-27 11:18:22 -0800

    yes

--- practique_de_function.py
+++ practique_de_function.py
@@ -1,8 +1,24 @@
-def stars(l: int) -> str:
-    pyramid = ""
-    for n in range(1, l+1):
-        if (n<l):
-            pyramid += "*" * n + "\n"
-        else: pyramid += "*" * n
-    return pyramid
-print(stars(6))
+# def triangle(l: int) -> str:
+#     pyramid = ""
+#     for n in range(1, l+1):
+#         if (n<l):
+#             pyramid += "*" * n + "\n"
+#         else: pyramid += "*" * n
+#     return pyramid
+# print(triangle(6))
+
+# def max(a: int, b: int, c: int) -> int:
+#     if a > b and a > c:
+#         return a
+#     if c > b and c > a:
+#         return c
+#     if b > a and b > c:
+#         return b
+# print(max(2,3,4))
+
+# def stars(l: int) -> str:
+#     star = ""
+#     star += "*" * l
+#     return star
+# print(stars(7))
+
