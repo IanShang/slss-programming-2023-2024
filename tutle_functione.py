d607c04d12cb885c038d14c6e115c2a13057d643
-v <vipianshang@gmail.com>
2023-11-30 10:21:07 -0800

    5^4

--- tutle_functione.py
+++ tutle_functione.py
@@ -4,19 +4,25 @@
 
 import turtle
 
+screen = turtle.Screen()
+screen.setup(10000, 10000)
+
 el_gato = turtle.Turtle()
 el_gato.color("orange")
 el_gato.shape("turtle")
+el_gato.speed(0)
 
 def square(n: float):
     # returne quand nn
     return n**2
 
-def dessiner_square(l: float):
+def dessiner_carree(l: float):
     for n in range(4):
         el_gato.forward(l)
         el_gato.right(90)
 
-turtle.done()
+dessiner_carree(square(5))
+dessiner_carree(square(square(5)))
+dessiner_carree(square(square(square(5))))
 
-dessiner_square(square(5))
+turtle.done()
