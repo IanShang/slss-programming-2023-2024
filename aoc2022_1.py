c8e7b36ff13e4407f09c5296a6c46b96e74f5a14
-v <vipianshang@gmail.com>
2023-12-04 11:14:18 -0800

    sada andagi

--- aoc2022_1.py
+++ aoc2022_1.py
@@ -8,8 +8,25 @@ J_maintenant = 0
 with open("./aoc2022_1.txt") as f:
 
     for line in f:
-        #si il y a une item dans le ligne
+        # si il y a une item dans le ligne
         if(line.strip()):
             J_maintenant += int(line.strip())
+        else:
+            # si il n'y a pas une numero
+            # plus le numero
+            elves_inventoire.append(J_maintenant)
+            # changer J_maintenant a 0
+            J_maintenant = 0
     
-print(J_maintenant)
+    grande = 0
+    i = 1
+    # for J in range(len(elves_inventoire)):
+    while i < len(elves_inventoire):
+        # this is a certified java coder moment
+        if elves_inventoire[i] > grande:
+            grande = elves_inventoire[i]
+        i+=1
+
+print(elves_inventoire)
+print(grande)
+# print(str(sum(sorted(elves_inventoire[-3:]))))
--- tutle_functione.py
+++ tutle_functione.py
@@ -12,17 +12,47 @@ el_gato.color("orange")
 el_gato.shape("turtle")
 el_gato.speed(0)
 
-def square(n: float):
-    # returne quand nn
-    return n**2
+# def square(n: float):
+#     # returne quand nn
+#     return n**2
 
-def dessiner_carree(l: float):
-    for n in range(4):
+# def dessiner_carree(l: float):
+#     for n in range(4):
+#         el_gato.forward(l)
+#         el_gato.right(90)
+
+# dessiner_carree(square(5))
+# dessiner_carree(square(square(5)))
+# dessiner_carree(square(square(square(5))))
+
+def dessine_arbre(level: int, l: int):
+    # c'est ap comp sci flashbacks (i actually did ok in ap comp sci)
+    # dessiner arbre avec y que est me donne
+    # param =
+    # level que me parler le level de branch
+    # height y de arbres dans pixel
+
+    if level > 0: #🤔
+        # dessiner les branch
         el_gato.forward(l)
+        # turner a port
+        el_gato.left(45)
+        # dessiner une arbres petit: level - 1
+        dessine_arbre(level - 1, l / 2)
+        # turner a starboard
         el_gato.right(90)
+        # dessiner une arbres petit: level - 1
+        dessine_arbre(level - 1, l / 2)
+        # aller a la origin
+        el_gato.left(45)
+        el_gato.back(l)
+    else:
+        # faire level 0 arbre, une leaf desune
+        color_originale = el_gato.color()
+        el_gato.color("green")
+        el_gato.stamp()
 
-dessiner_carree(square(5))
-dessiner_carree(square(square(5)))
-dessiner_carree(square(square(square(5))))
+el_gato.setheading(90)
+dessine_arbre(4, 250)
 
 turtle.done()
