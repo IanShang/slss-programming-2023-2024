475d0dae2004bae55bee71f37d40349e3b0ba0cc
-v <vipianshang@gmail.com>
2023-12-07 10:54:38 -0800

    drugs

--- Notes/function.py
+++ Notes/function.py
@@ -3,14 +3,29 @@
 # nous pouvons referee les data a parametre
 # nous utilisons "arguments" au describe data
 
-def photocopier_area_de_square(s: float) -> None:
-    print("l'area de square est " + str(s*2) + " sqaured")
+# def photocopier_area_de_square(s: float) -> None:
+#     print("l'area de square est " + str(s*2) + " sqaured")
 
-def area_de_square(s: float) -> float:
-    area = s ** 2
-    return area
+# def area_de_square(s: float) -> float:
+#     area = s ** 2
+#     return area
 
-photocopier_area_de_square(2)
-area_de_square(2)
+# photocopier_area_de_square(2)
+# area_de_square(2)
 
-plus_de_area = area_de_square(1) + area_de_square(1)
+# plus_de_area = area_de_square(1) + area_de_square(1)
+
+# # function que retourne
+# def plus(x: int, y:int) -> int:
+#     sum = x+y
+#     return sum # arrete le function, si il y a une val apres, il donne le val ou le function a telephone
+
+# plus(1, 1)
+
+def trouver_linear(l: list, item: any) -> int:
+    for n in range(len(l)):
+        if item == l[n]:
+            return n
+    return -100000
+
+print("je trouve ton drugs a la numero " + str(trouver_linear(["doragusu", "drugs"], "drugs")) + " place")
--- tutle_functione.py
+++ tutle_functione.py
@@ -2,6 +2,61 @@
 # author
 # 28/11/23
 
+# import turtle
+
+# screen = turtle.Screen()
+# screen.setup(10000, 10000)
+
+# el_gato = turtle.Turtle()
+# el_gato.color("orange")
+# el_gato.shape("turtle")
+# el_gato.speed(0)
+
+# # def square(n: float):
+# #     # returne quand nn
+# #     return n**2
+
+# # def dessiner_carree(l: float):
+# #     for n in range(4):
+# #         el_gato.forward(l)
+# #         el_gato.right(90)
+
+# # dessiner_carree(square(5))
+# # dessiner_carree(square(square(5)))
+# # dessiner_carree(square(square(square(5))))
+
+# def dessine_arbre(level: int, l: int):
+#     # c'est ap comp sci flashbacks (i actually did ok in ap comp sci)
+#     # dessiner arbre avec y que est me donne
+#     # param =
+#     # level que me parler le level de branch
+#     # height y de arbres dans pixel
+
+#     if level > 0: #🤔
+#         # dessiner les branch
+#         el_gato.forward(l)
+#         # turner a port
+#         el_gato.left(45)
+#         # dessiner une arbres petit: level - 1
+#         dessine_arbre(level - 1, l / 2)
+#         # turner a starboard
+#         el_gato.right(90)
+#         # dessiner une arbres petit: level - 1
+#         dessine_arbre(level - 1, l / 2)
+#         # aller a la origin
+#         el_gato.left(45)
+#         el_gato.back(l)
+#     else:
+#         # faire level 0 arbre, une leaf desune
+#         color_originale = el_gato.color()
+#         el_gato.color("green")
+#         el_gato.stamp()
+
+# el_gato.setheading(90)
+# dessine_arbre(4, 250)
+
+# turtle.done()
+
 import turtle
 
 screen = turtle.Screen()
@@ -12,20 +67,7 @@ el_gato.color("orange")
 el_gato.shape("turtle")
 el_gato.speed(0)
 
-# def square(n: float):
-#     # returne quand nn
-#     return n**2
-
-# def dessiner_carree(l: float):
-#     for n in range(4):
-#         el_gato.forward(l)
-#         el_gato.right(90)
-
-# dessiner_carree(square(5))
-# dessiner_carree(square(square(5)))
-# dessiner_carree(square(square(square(5))))
-
-def dessine_arbre(level: int, l: int):
+def dessine_arbre_beau(level: int, l: int): #c'est une belle arbre, n'est pas bizarre
     # c'est ap comp sci flashbacks (i actually did ok in ap comp sci)
     # dessiner arbre avec y que est me donne
     # param =
@@ -38,21 +80,45 @@ def dessine_arbre(level: int, l: int):
         # turner a port
         el_gato.left(45)
         # dessiner une arbres petit: level - 1
-        dessine_arbre(level - 1, l / 2)
+        dessine_arbre_beau(level - 1, l / 2)
         # turner a starboard
         el_gato.right(90)
         # dessiner une arbres petit: level - 1
-        dessine_arbre(level - 1, l / 2)
+        dessine_arbre_beau(level - 1, l / 2)
         # aller a la origin
         el_gato.left(45)
-        el_gato.back(l)
+        # dessine mid branch desune
+        dessine_arbre_beau(level - 1, l / 2)
+        el_gato.back(l * 2)
+        el_gato.left(90)
+        # funny
+        dessine_arbre_beau(level - 1, l / 2)
+        el_gato.back(l * 2)
+        el_gato.left(45)
+        # funny
+        dessine_arbre_beau(level - 1, l / 2)
+        el_gato.back(l * 2)
+        el_gato.left(45)
+        # funny
+        dessine_arbre_beau(level - 1, l / 2)
+        el_gato.back(l * 2)
+        el_gato.left(45)
+        # funny
+        dessine_arbre_beau(level - 1, l / 2)
+        el_gato.back(l * 1.5)
+        el_gato.left(45)
+        # funny
+        dessine_arbre_beau(level - 1, l / 2)
+        el_gato.back(l * 3)
+
     else:
         # faire level 0 arbre, une leaf desune
         color_originale = el_gato.color()
         el_gato.color("green")
         el_gato.stamp()
+        el_gato.color(color_originale[0])
 
 el_gato.setheading(90)
-dessine_arbre(4, 250)
+dessine_arbre_beau(5, 75)
 
 turtle.done()
